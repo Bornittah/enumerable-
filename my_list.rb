647f449c06e8a5c129fd26683b91b96cee193e1c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-11 01:46:31 +0430

    Test the provided test-cases

--- my_list.rb
+++ my_list.rb
@@ -15,3 +15,13 @@ class MyList
     end
   end
 end
+
+# test the provided test-cases
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
