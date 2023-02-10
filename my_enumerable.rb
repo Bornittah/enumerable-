ea82c8f4830fdb77a98ffa3ea97000fa4a63eba9
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-11 01:47:24 +0430

    Add module MyEnumbrable

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,4 +1,20 @@
 module MyEnumerable
-#  write your code here
-
-end
+    def all?
+      check_arr = []
+      @list.each { |num| check_arr.push(num) if yield num }
+      check_arr.length == @list.length
+    end
+  
+    def any?
+      if_any = false
+      @list.each { |num| if_any = true if yield num }
+      if_any
+    end
+  
+    def filter
+      filtered_arr = []
+      @list.each { |num| filtered_arr.push(num) if yield num }
+      filtered_arr
+    end
+  end
+  
